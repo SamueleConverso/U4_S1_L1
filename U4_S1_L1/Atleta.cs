ed722857ee1eb772742f8d69589ff844953ae293
Samuele Converso <samu.converso@gmail.com>
2025-02-10 15:19:03 +0100

    .

---
+++ U4_S1_L1/Atleta.cs
@@ -0,0 +1,22 @@
+﻿namespace U4_S1_L1
+{
+    internal class Atleta
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        public int Age { get; set; }
+
+        public Atleta(string firstName, string lastName, int age)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            Age = age;
+        }   
+
+        public void ShowData()
+        {
+            Console.WriteLine("Nome: " + FirstName + " | Cognome: " + LastName + " | Età: " + Age);
+        }
+    }
+}
--- U4_S1_L1/Program.cs
+++ U4_S1_L1/Program.cs
@@ -1,1 +1,5 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using U4_S1_L1;
+
+var atleta1 = new Atleta("Juan", "Perez", 26);
+
+atleta1.ShowData();
